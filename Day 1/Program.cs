f0215cd3fe72d9adaddc05ade402ea290f082e05
MrZombieCow <bjornar.andreassen.elektro@gmail.com>
2021-12-06 22:55:58 +0100

    Part 1 and Part 2

--- Day 1/Program.cs
+++ Day 1/Program.cs
@@ -1,22 +1,46 @@
-﻿int counter = 0;
-
-var file = File.ReadAllLines(path: "input.txt");
-
-var prev_num1 = int.Parse(file[0]);
-var prev_num2 = int.Parse(file[1]);
-var prev_num3 = int.Parse(file[2]);
-
-for (int i = 3; i < file.Length; i++)
+﻿int Part1()
 {
-    var curr_num = int.Parse(file[i]);
+    int counter = 0;
 
-    if (prev_num1 + prev_num2 + prev_num3 < curr_num + prev_num2 + prev_num3)
-        counter++;
+    var file = File.ReadAllLines(path: "input.txt");
 
-    prev_num1 = prev_num2;
-    prev_num2 = prev_num3;
-    prev_num3 = curr_num;
+    var prev_num = int.Parse(file[0]);
+
+    for (int i = 1; i < file.Length; i++)
+    {
+        var curr_num = int.Parse(file[i]);
+
+        if (curr_num > prev_num)
+            counter++;
+        prev_num = curr_num;
+    }
+    return counter;
 }
 
-Console.WriteLine("There are greater {0} lines", counter);
+int Part2()
+{
+    int counter = 0;
+
+    var file = File.ReadAllLines(path: "input.txt");
+
+    var prev_num1 = int.Parse(file[0]);
+    var prev_num2 = int.Parse(file[1]);
+    var prev_num3 = int.Parse(file[2]);
+    for (int i = 3; i < file.Length; i++)
+    {
+        var curr_num = int.Parse(file[i]);
+
+        if (prev_num1 + prev_num2 + prev_num3 < curr_num + prev_num2 + prev_num3)
+            counter++;
+
+        prev_num1 = prev_num2;
+        prev_num2 = prev_num3;
+        prev_num3 = curr_num;
+    }
+
+    return counter;
+}
+
+Console.WriteLine("Part 1 has {0} greater numbers", Part1());
+Console.WriteLine("Part 2 has {0} greater lines", Part2());
 Console.ReadLine();
