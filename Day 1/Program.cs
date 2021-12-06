8abc0c7248827d6dd2c40f19883c2ec7c1a9dab0
MrZombieCow <MrZombieCow@MRZOMBIECOW-PC>
2021-12-06 22:04:35 +0100

    Add project files.

---
+++ Day 1/Program.cs
@@ -0,0 +1,18 @@
+﻿int counter = 0;
+int curr_num = 0;
+int prev_num = int.MaxValue;
+
+
+
+foreach (var line in File.ReadLines(path: "input.txt"))
+{
+    curr_num = int.Parse(line);
+
+    if (curr_num > prev_num)
+        counter++;
+    prev_num = curr_num;
+}
+
+Console.WriteLine("There are greater {0} lines", counter);
+Console.ReadLine();
+
