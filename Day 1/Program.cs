04a9562cab292162a004ab9edcc7fa848baff600
MrZombieCow <bjornar.andreassen.elektro@gmail.com>
2021-12-06 22:45:18 +0100

    Part 2 Done

--- Day 1/Program.cs
+++ Day 1/Program.cs
@@ -1,18 +1,22 @@
 ﻿int counter = 0;
-int curr_num = 0;
-int prev_num = int.MaxValue;
 
+var file = File.ReadAllLines(path: "input.txt");
 
+var prev_num1 = int.Parse(file[0]);
+var prev_num2 = int.Parse(file[1]);
+var prev_num3 = int.Parse(file[2]);
 
-foreach (var line in File.ReadLines(path: "input.txt"))
+for (int i = 3; i < file.Length; i++)
 {
-    curr_num = int.Parse(line);
+    var curr_num = int.Parse(file[i]);
 
-    if (curr_num > prev_num)
+    if (prev_num1 + prev_num2 + prev_num3 < curr_num + prev_num2 + prev_num3)
         counter++;
-    prev_num = curr_num;
+
+    prev_num1 = prev_num2;
+    prev_num2 = prev_num3;
+    prev_num3 = curr_num;
 }
 
 Console.WriteLine("There are greater {0} lines", counter);
 Console.ReadLine();
-
